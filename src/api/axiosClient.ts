6f216a0b9f525cbda98f5f914b48dbb57cd76d3c
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2026-01-07 01:06:48 +0530

    testing prod

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL:  import.meta.env.URL || "http://localhost:4000/api",
+  baseURL:  import.meta.env.URL,
   withCredentials: true, // VERY IMPORTANT for cookies
 });
